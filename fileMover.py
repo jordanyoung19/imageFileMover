86c2a0bc3d1b22785eb1e6cfa13007e5a82acaae
Jordan Young <jordanby@uci.edu>
2020-06-15 21:44:14 -0700

    "first draft of script"

--- fileMover.py
+++ fileMover.py
@@ -1,3 +1,17 @@
 import time
 import shutil
+import os
 
+downloadsPath = "/Users/jordanyoung/Downloads"
+airDropPath = "/Users/jordanyoung/Pictures/airDrop"
+
+def running():
+    running = True
+    while running:
+        for filename in os.listdir(downloadsPath):
+            if filename.endswith(".heic"):
+                shutil.move(filename, airDropPath)
+        time.sleep(2)
+    
+if __name__ == "__main__":
+    running()
